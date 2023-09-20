0808acd31a4a64fc16d49ec3fdf88462aa507272
Vlad1364 <108176399+Vlad1364@users.noreply.github.com>
2023-09-20 13:41:35 +0300

    2

---
+++ lab02/lab_2_BarbirVladyslav.py
@@ -0,0 +1,15 @@
+print("Programming", "Essential", "in...",sep="***",end="")
+print("Python",)
+
+print("      *","\n     * *","\n    *   * ","\n   *     *","\n  ***   *** ","\n    *   *","\n    *   *","\n    *****")
+
+
+print("I\'m student")
+
+
+print("\"I\'m\"","\n\"\"learning\"\"","\n\"\"\"Python\"\"\"")
+
+print(0o500)
+
+print(0x777)
+
